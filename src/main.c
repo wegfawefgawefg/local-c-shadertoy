c0b13ceafe4dc79700b5db5f7e51e14121243c66
wegfawefgaewfg <668es218pur@gmail.com>
2024-08-12 00:40:11 +0900

    got some uniforms going in

--- src/main.c
+++ src/main.c
@@ -10,6 +10,10 @@
 #include "state.h"
 #include "input.h"
 
+// const of the dims
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
@@ -20,15 +24,15 @@ int main(int argc, char *argv[])
     SDL_Window *window = SDL_CreateWindow(
         "Full Screen Shader",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        800, 600,
+        WIDTH, HEIGHT,
         SDL_WINDOW_OPENGL);
 
-    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
 
-    GLuint shaderProgram = createShaderProgram();
-    if (shaderProgram == 0)
+    GLuint shader_program = createShaderProgram();
+    if (shader_program == 0)
     {
-        SDL_GL_DeleteContext(glContext);
+        SDL_GL_DeleteContext(gl_context);
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
@@ -60,7 +64,23 @@ int main(int argc, char *argv[])
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Use the shader program
-        glUseProgram(shaderProgram);
+        glUseProgram(shader_program);
+
+        // pass in dims of window
+        GLint dims_location = glGetUniformLocation(shader_program, "dims");
+        glUniform2f(dims_location, WIDTH, HEIGHT);
+
+        // pass in time
+        GLint time_location = glGetUniformLocation(shader_program, "time");
+        glUniform1f(time_location, SDL_GetTicks() / 1000.0f);
+
+        // pass in mouse
+        int x, y;
+        SDL_GetMouseState(&x, &y);
+        GLint location = glGetUniformLocation(shader_program, "mouse");
+        float mouse_norm_x = (float)x / (float)WIDTH;
+        float mouse_norm_y = (float)y / (float)HEIGHT;
+        glUniform2f(location, mouse_norm_x, mouse_norm_y);
 
         // Draw the full-screen quad
         glBindVertexArray(VAO);
@@ -73,8 +93,8 @@ int main(int argc, char *argv[])
     // Clean up
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
-    SDL_GL_DeleteContext(glContext);
+    glDeleteProgram(shader_program);
+    SDL_GL_DeleteContext(gl_context);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
--- src/shaders/fragment_shader.glsl
+++ src/shaders/fragment_shader.glsl
@@ -1,7 +1,16 @@
 #version 330 core
 out vec4 FragColor;
 
+uniform float time;
+uniform vec2 dims;
+uniform vec2 mouse;
+
 void main()
 {
-    FragColor = vec4(gl_FragCoord.x / 800.0, gl_FragCoord.y / 600.0, 0.5, 1.0);
+    vec2 uv = gl_FragCoord.xy / dims;
+    uv.y = 1.0 - uv.y;
+
+    float dist = distance(uv, mouse);
+    dist = sin(dist * 10.0 + time) * 0.5 + 0.5;
+    FragColor = vec4(dist, dist, dist, 1.0);
 }
